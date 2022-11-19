d641259b833b2a35f7d2289f09af7765c4982284
Ruslan <shamiltat79@gmail.com>
2022-11-19 23:58:26 +0200

    hw10

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 import './css/styles.css';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import { fetchCountries } from './fetchCountry';
+
+let debounce = require('lodash.debounce');
 
 const DEBOUNCE_DELAY = 300;
+const searchBox = document.querySelector('#search-box');
+console.log(searchedCountry);
+const countryList = document.querySelector('.country-list');
+const countryData = document.querySelector('.country-info');
+searchBox.addEventListener('input', debounce(searchedCountry, DEBOUNCE_DELAY));
+
+function searchedCountry() {
+ const Country = event.target.value;
+
+    clearPreviousInfo();
+
+    if (Country === '') {
+        return;
+    };
+};
+ 
