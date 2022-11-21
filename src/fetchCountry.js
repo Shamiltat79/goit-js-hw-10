3591557ca6e9c4a3320c6059a1c10b781f023d30
Ruslan <shamiltat79@gmail.com>
2022-11-21 05:14:13 +0200

    refactoping code

--- src/fetchCountry.js
+++ src/fetchCountry.js
@@ -1,49 +1,11 @@
-import { Notify } from 'notiflix/build/notiflix-notify-aio';
-
-const countryList = document.querySelector('.country-list');
-const countryData = document.querySelector('.country-info');
-
 export function fetchCountries(name) {
-    fetch(`https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`)
+   return fetch(`https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`)
         .then(response => {
             if (!response.ok) {
-                throw new Error(response.status);
+        throw new Error(response.status);        
             }
-            return response.json();
+            return response.json();  
         })
-        .then(data => {
-            let countries = data;
-            for (let { name: { official: countryName }, capital, population, flags: { svg: flag }, languages } of countries) {
-                if (countries.lenght > 1 && countries.lenght < 11) {
-                    countryList.insertAdjacentHTML('beforeend', `
-                    <li>
-                    <img src="${flag}", width="70"></img>
-                    <span class="country-info__entry">${countryName}</span>
-                    </li>`);
-                } else if (countries.length === 1) {
-                    countryData.insertAdjacentHTML('beforeend', `
-                        <div class="country-info">
-                        <img src="${flag}", width="70"></img>
-                        <span class="country-info">${countryName}</span>
-                        </div>
-                        <p><span class="country-info">Capital:</span> ${capital}</p>
-                        <p><span class="country-info">Population:</span> ${population}</p>
-                        <p><span class="country-info">Languages:</span> ${Object.values(languages)}</p>
-                        `)
-           
-                }
-                else {
-                    Notify.info("Too many matches found. Please enter a more specific name.");
-                    return;
-                }
-            }
-        })
-           
-        .catch ((error) => {
-            
-                Notify.failure('Oops, there is no country with that name');
-            
-            })
+       
+       .catch(error => { console.log('Error!') }); 
 };
-
-
--- src/index.js
+++ src/index.js
@@ -1,25 +1,74 @@
+import debounce from 'lodash.debounce';
 import './css/styles.css';
 import { fetchCountries } from './fetchCountry';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
-let debounce = require('lodash.debounce');
+
 const DEBOUNCE_DELAY = 300;
-const searchBox = document.querySelector('#search-box');
-const countryList = document.querySelector('.country-list');
-const countryData = document.querySelector('.country-info');
-
-searchBox.addEventListener('input', debounce(searchedCountry, DEBOUNCE_DELAY));
-
-function searchedCountry(event) {
-    countryList.innerHTML = "";
-    countryData.innerHTML = "";
-    const Country = event.target.value.trim();;
-
-    
-
-    if (Country !== '') {
-        fetchCountries(Country);
-    }
+const refs = {
+  searchBox: document.querySelector('#search-box'),
+  countryList: document.querySelector('.country-list'),
+  countryInfo: document.querySelector('.country-info'),
 };
 
+refs.searchBox.addEventListener('input',debounce(onInput, DEBOUNCE_DELAY));
 
+function onInput(event) {
+    event.preventDefault();
+    const countryToSearch = event.target.value.trim();
+    refs.countryInfo.innerHTML = '';
+    refs.countryList.innerHTML = '';
+    if (!countryToSearch) {
+        return;
+    }
+    fetchCountries(countryToSearch)
+        .then(countries => {
+            if (countries.lenght > 10) {
+                return Notify.info('Too many matches found. Please enter a more specific name.');
+            }
+            
+            if (countries.lenght === 1) {
+        createContryList(countries);
+            }
+            createCountrtiesList(countries);
+        })
+    .catch(error => Notify.failure('Oops, there is no country with that name'));
+    
+
+};
+
+ 
+function createContryList(country) {
+    const markup = country.map(({ flags, name, capital, population, languages }) => {
+       return `<img width="60px" height="50px" src='${flags.svg}' />
+        <ul class="country-info__list">
+            <li class="country-info__item country-info__item--name"><p><b>Name: </b>${
+              name.official
+            }</p></li>
+            <li class="country-info__item"><p><b>Capital: </b>${capital}</p></li>
+            <li class="country-info__item"><p><b>Population: </b>${population}</p></li>
+            <li class="country-info__item"><p><b>Languages: </b>${Object.values(
+              languages
+            )}</p></li>
+        </ul>`;
+    })  
+    .join('');
+  refs.countryInfo.innerHTML = markup;
+}
+
+
+function createCountrtiesList(countries) {
+    const markup = countries
+    .map(({ flags, name }) => {
+      return `
+                <li class="country-list__item">
+                    <img class="country-list__flag" src="${flags.svg}" width = 60px height = 50px>
+                    <p class="country-list__name">${name.official}</p>
+                </li>
+                `;
+    })
+    .join('');
+
+  refs.countryInfo.innerHTML = markup;
+};
 
